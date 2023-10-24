0aba63ace84d164492dee585da046b9f18a966d8
dennislee22 <35444414+dennislee22@users.noreply.github.com>
2023-10-24 13:26:44 +0800

    Update gdio.py

--- gdio.py
+++ gdio.py
@@ -11,15 +11,27 @@ def flush_gpu_memory():
     tokenizer = model = None
     torch.cuda.empty_cache()
 
-def reload_model(model_name):
-    #return f"{model_name}"
+def load_model(model_name):
     global selected_model  # Access the selected_model variable
     selected_model = model_name
-    tokenizer = AutoTokenizer.from_pretrained(model_name,torch_dtype=torch.bfloat16,device_map="auto")
-    model = AutoModelForCausalLM.from_pretrained(model_name,torch_dtype=torch.bfloat16,device_map="auto")
-    loaded_models[model_name] = {"tokenizer": tokenizer, "model": model}
+    if selected_model == []:
+        yield f"No model selected"
+        print("No model selected")
+        return
+    else:   
+        if selected_model != []:
+                yield f"Selected model is `{selected_model}`"
+                time.sleep(2)
+                yield f"Loading `{selected_model}`..."
+                tokenizer = AutoTokenizer.from_pretrained(model_name,torch_dtype=torch.bfloat16,device_map="auto")
+                model = AutoModelForCausalLM.from_pretrained(model_name,torch_dtype=torch.bfloat16,device_map="auto")
+                loaded_models[model_name] = {"tokenizer": tokenizer, "model": model}
+                yield f"Successfully loaded `{selected_model}`."
+        else:
+            yield f"Failed to load `{selected_model}`."
     
-def generate_response(input_text):
+def generate_response(input_text,output_text):
+#def generate_response(input_text, _):
         if selected_model not in loaded_models:
             return "Model not loaded. Click the 'Reload Model' button to load a model."
         tokenizer = loaded_models[selected_model]["tokenizer"]
@@ -30,24 +42,29 @@ def generate_response(input_text):
         return response_text
 
 def create_ui():
-    with gr.Tab("tab1"):
+    with gr.Tab("AI Text Generator"):
         with gr.Row():
             with gr.Column():
-                model_selected = gr.Dropdown(choices=["falcon-7b", "dlee-falcon-7b-fine-tuned"], label='Choose a GenAI Model')        
+                model_selected = gr.Dropdown(choices=["falcon-7b", "dlee-falcon-7b-fine-tuned"], label='Select a GenAI Model')        
                 reload_button = gr.Button("Reload Model")
+                status_message = gr.Label(label="Model Status")
                 inp = model_selected
-                out = gr.Textbox()
-                reload_button.click(flush_gpu_memory).then(fn=reload_model, inputs=inp)
+                reload_button.click(flush_gpu_memory).then(load_model, inputs=inp, outputs=status_message)
+                
                 with gr.Row():
                     with gr.Column():
                         global iface  
                         iface = gr.Interface(
                         fn=generate_response,
-                        inputs="text",
+                        inputs="text",  
                         outputs="text",
+                        title="LLM Chatbox",
+                        description="Enter a message to chat with the loaded model.",    
                         )
-                            
-with gr.Blocks(title="Chatbox using Fine-tuned Falcon 7B Model with Custom Dataset", theme=gr.themes.Default()) as demo:
+
+#Chatbox using Fine-tuned Falcon 7B Model with Custom Dataset
+with gr.Blocks(title="My Chatbox", theme=gr.themes.Soft()) as demo:
     create_ui()
+demo.queue()
 
 demo.launch(server_name="127.0.0.1", server_port=8090)
